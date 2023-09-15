db0a456bdb38594384017f92c7fbda62c48250af
jbanghw <jbanghw@gmail.com>
2023-09-15 03:34:33 -0400

    finished section 15

---
+++ Section15-Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by James Bang on 2023-09-14.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String
+    let body: String
+}
